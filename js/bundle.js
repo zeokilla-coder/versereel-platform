3b4434005ec31e95396d0bacb40d4eb2be911f99
VerseReel Creator <creator@versereel.com>
2026-08-16 09:11:55 -0500

    Update downloadUrl exact filename to assets/NO INTERNET.pdf v20.0

--- js/bundle.js
+++ js/bundle.js
@@ -37,7 +37,7 @@
       previewLimit: 20,
       paymentUrl: "https://mpago.la/2om6XKk",
       paypalUrl: "https://www.paypal.com/invoice/p/#4EV2U6E59BUXU3C6",
-      downloadUrl: "assets/NO_INTERNET.pdf",
+      downloadUrl: "assets/NO INTERNET.pdf",
       thumbnail: "assets/0.jpg",
       pages: Array.from({ length: 31 }, (_, i) => `assets/${i}.jpg`),
       views: 142,
